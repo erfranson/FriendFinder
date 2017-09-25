341f4235cdb81b742883681444f85055ae98aadf
Ethan Franson <ethanfranson2@gmail.com>
2017-09-25 13:06:23 -0600

    adding some of the api functions

---
+++ app/data/friend.js
@@ -0,0 +1,23 @@
+
+
+var friends = {
+
+ name: "Ethan",
+ photo: "friends",
+ scores: [
+	 5,
+	 1,
+ 	 4,
+ 	 4,
+ 	 5,
+     1,
+ 	 2,
+     5,
+     4,
+     1
+ ]
+
+};
+
+
+module.exports = friends;
--- app/routing/apiRouter.js
+++ app/routing/apiRouter.js
@@ -1,7 +1,11 @@
 var path = require("path");
+
+var friends = require("../data/friend.js");
+
+// exports api routes 
 module.exports = function(app) {
 	app.get("/api/friends", function (req, res){
-		res.send('test');
+		res.json(friends);
 	});
 
 }
